c38e6a640468ff49424c6f1198afce319db3421b
soffanderson <s.btudz@gmail.com>
2023-05-18 22:15:52 +0800

    Selesai melakukan request dari API, dan menampilkannya di console.log

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,18 @@
+import TheMovieDbSource from '../../data/themoviedb-source';
+import UrlParser from '../../routes/url-parser';
+
+const Detail = {
+  async render() {
+    return `
+    <h2>Detail Page</h2>
+    `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const movie = await TheMovieDbSource.detailMovie(url.id);
+    console.log(movie);
+  },
+};
+
+export default Detail;
